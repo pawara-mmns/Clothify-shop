7f12bbcfb88bc3981c3ccbd8e5a7785da2d24bf1
Pawara Samarawickrama <pawarasamarawickrama@gmail.com>
2024-10-25 13:53:19 +0530

    Daoimpl implement done

--- src/main/java/repository/custom/impl/EmployeeDaoImpl.java
+++ src/main/java/repository/custom/impl/EmployeeDaoImpl.java
@@ -1,6 +1,31 @@
 package repository.custom.impl;
 
+import javafx.collections.ObservableList;
 import repository.custom.EmployeeDao;
 
 public class EmployeeDaoImpl implements EmployeeDao {
+    @Override
+    public boolean save(Object o) {
+        return false;
+    }
+
+    @Override
+    public ObservableList getAll() {
+        return null;
+    }
+
+    @Override
+    public boolean delete(String id) {
+        return false;
+    }
+
+    @Override
+    public Object search(String id) {
+        return null;
+    }
+
+    @Override
+    public boolean update(Object o) {
+        return false;
+    }
 }
